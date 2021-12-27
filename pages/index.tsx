2a30ffceb2dbdf03b3adfd643e25849c7aff2958
tchojnacki <tomaszchojnacki2001@gmail.com>
2021-12-27 13:09:18 +0100

    Add social buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { siGithub, siLinkedin } from 'simple-icons/icons'
 import styles from '@/styles/about/About.module.css'
 import Nav from '@/components/common/Nav'
 import TechSphere from '@/components/about/TechSphere'
 import Wave from '@/components/about/Wave'
 import LinkButton from '@/components/about/LinkButton'
+import SimpleIconSvg from '@/components/common/SimpleIconSvg'
 
 const About: NextPage = () => {
   return (
@@ -21,6 +23,12 @@ const About: NextPage = () => {
           <p className={styles.heroParagraph}>I am a software developer.</p>
           <div className={styles.heroButtons}>
             <LinkButton href="/projects">View Projects</LinkButton>
+            <a className={styles.heroSocialLink} href="https://github.com/tchojnacki">
+              <SimpleIconSvg icon={siGithub} size="2rem" />
+            </a>
+            <a className={styles.heroSocialLink} href="https://www.linkedin.com/in/tchojnacki">
+              <SimpleIconSvg icon={siLinkedin} size="2rem" />
+            </a>
           </div>
         </section>
         <section className={styles.sphereWrapper}>
