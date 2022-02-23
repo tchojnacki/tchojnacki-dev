7e6e8dc2d53f09e366acc5eadae1ff7427672f5a
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-02-23 15:41:56 +0100

    Extract typewriter consts

--- components/about/HeroTypewriter.tsx
+++ components/about/HeroTypewriter.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { useTypewriter } from '@/util/hooks/useTypewriter'
 import styles from '@/styles/about/HeroTypewriter.module.scss'
 
+const TYPEWRITER_TEXTS = ['software engineer.', 'student.', 'frontend developer.']
+
 const HeroTypewriter = () => {
-  const text = useTypewriter(['software engineer.', 'student.', 'frontend developer.'], {
+  const text = useTypewriter(TYPEWRITER_TEXTS, {
     type: 100,
     beforeTyping: 500,
     beforeErasing: 3000,
