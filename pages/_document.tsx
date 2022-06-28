209d870dd7cc2bc848ea5c3d1c0d7915b2581286
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-06-28 17:29:25 +0200

    fix: use font stack while Inter is not available

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
