17bdf0f9faf57c5deb984a4ab1b46e50552d49a2
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-06-29 22:21:52 +0200

    build: add next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const defaultConfig = {
   reactStrictMode: true,
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(defaultConfig)
