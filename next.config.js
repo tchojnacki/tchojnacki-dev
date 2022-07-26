56f3a4f543c6cd899d0b7fbf0bd019426b98fde6
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-07-26 12:40:36 +0200

    feat: replace React with Preact in production

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 // @ts-check
 
 /** @type {import('next').NextConfig} */
-const defaultConfig = {
+const baseConfig = {
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Source: https://darrenwhite.dev/blog/nextjs-replace-react-with-preact
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer(defaultConfig)
+module.exports = withBundleAnalyzer(baseConfig)
