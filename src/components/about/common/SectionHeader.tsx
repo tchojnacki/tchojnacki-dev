34d980dc8931d8c16ac8069ee022c068b0b36550
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-07-28 21:28:32 +0200

    feat(about): display link icon on section header hover

--- src/components/about/common/SectionHeader.tsx
+++ src/components/about/common/SectionHeader.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'tabler-icons-react'
+
 interface SectionHeaderProps {
   children: string
   slug?: string
@@ -9,8 +11,14 @@ export function SectionHeader({ children, slug }: SectionHeaderProps) {
   const id = slug ?? textToSlug(children)
 
   return (
-    <h3 id={id} className="text-center text-4xl font-bold capitalize pt-4">
-      <a href={`#${id}`}>{children}</a>
+    <h3
+      id={id}
+      className="text-center text-4xl font-bold capitalize pt-4 flex justify-center items-center"
+    >
+      <a className="px-10 peer" href={`#${id}`}>
+        {children}
+      </a>
+      <Link className="-order-1 -mr-8 opacity-0 peer-hover:opacity-50 duration-200" />
     </h3>
   )
 }
