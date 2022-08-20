ddb4635a7f59b0d4c94a18fadf501152e17d1a5b
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-08-20 20:42:44 +0200

    feat(home): rotate TechSphere on drag

--- src/components/home/landing/TechSphere.tsx
+++ src/components/home/landing/TechSphere.tsx
@@ -1,11 +1,14 @@
 import clamp from 'lodash/clamp'
-import { useRef } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 
 import { TECH_SPHERE_SKILL_NAMES } from 'data'
 import { useAnimationFrame, useParentSize, usePrefersReducedMotion } from 'hooks'
 import {
   INITIAL_ROTATION_AXIS,
+  Pos3D,
+  findRotation,
   initialPositionsOf,
+  pointerToSpherePoint,
   rotateAroundUnitVector,
   worldToCamera,
 } from 'shared'
@@ -13,41 +16,111 @@ import {
 export function TechSphere() {
   const prefersReducedMotion = usePrefersReducedMotion()
   const { width, height, childRef: canvasRef } = useParentSize<HTMLCanvasElement>()
-  const size = clamp(Math.min(width, height) - 50, 200, 450)
-  const projection = { perspective: 2 * size, cx: size / 2, cy: size / 2 }
-  const sphereRadius = size * 0.35
-  const points = initialPositionsOf(TECH_SPHERE_SKILL_NAMES, sphereRadius)
+  const canvasSize = clamp(Math.min(width, height) - 50, 200, 450)
+  const cameraZ = 2 * canvasSize
+  const projection = useMemo(() => ({ cameraZ, canvasSize }), [cameraZ, canvasSize])
+  const sphereRadius = canvasSize * 0.35
+  const pointsRef = useRef(initialPositionsOf(TECH_SPHERE_SKILL_NAMES, sphereRadius))
 
-  const context = canvasRef.current?.getContext('2d')
+  const ctx = canvasRef.current?.getContext('2d')
 
-  const omega = prefersReducedMotion ? 0 : 0.5
+  const hoverPosRef = useRef({ x: 0, y: 0 })
+  const dragStartPosRef = useRef<Pos3D | null>(null)
+
+  const omegaRef = useRef(0)
   const thetaRef = useRef(0)
   const axisRef = useRef(INITIAL_ROTATION_AXIS)
 
+  useEffect(() => {
+    omegaRef.current = prefersReducedMotion ? 0 : 0.5
+  }, [prefersReducedMotion])
+
+  useEffect(() => {
+    pointsRef.current = initialPositionsOf(TECH_SPHERE_SKILL_NAMES, sphereRadius)
+  }, [sphereRadius])
+
+  useEffect(() => {
+    function moveHandler(event: PointerEvent) {
+      const rect = canvasRef.current!.getBoundingClientRect()
+      hoverPosRef.current = { x: event.clientX - rect.left, y: event.clientY - rect.top }
+
+      if (dragStartPosRef.current) {
+        const end = pointerToSpherePoint(hoverPosRef.current, sphereRadius, projection)
+        const { axis, theta } = findRotation(dragStartPosRef.current, end)
+        axisRef.current = axis
+        thetaRef.current = theta
+      }
+    }
+
+    function mouseDownHandler() {
+      dragStartPosRef.current = pointerToSpherePoint(hoverPosRef.current, sphereRadius, projection)
+      pointsRef.current = pointsRef.current.map(({ item, position }) => ({
+        item,
+        position: rotateAroundUnitVector(position, axisRef.current, thetaRef.current),
+      }))
+
+      omegaRef.current = 0
+      thetaRef.current = 0
+    }
+
+    function upHandler() {
+      dragStartPosRef.current = null
+      omegaRef.current = 1
+    }
+
+    function touchStartHandler(event: TouchEvent) {
+      event.preventDefault()
+      mouseDownHandler()
+    }
+
+    const target = canvasRef.current
+    if (target) {
+      target.addEventListener('pointermove', moveHandler)
+      target.addEventListener('mousedown', mouseDownHandler)
+      target.addEventListener('pointerup', upHandler)
+      target.addEventListener('pointerleave', upHandler)
+      target.addEventListener('touchstart', touchStartHandler)
+      return () => {
+        target.removeEventListener('pointermove', moveHandler)
+        target.removeEventListener('mousedown', mouseDownHandler)
+        target.removeEventListener('pointerup', upHandler)
+        target.removeEventListener('pointerleave', upHandler)
+        target.removeEventListener('touchstart', touchStartHandler)
+      }
+    }
+  }, [canvasRef, projection, sphereRadius])
+
   useAnimationFrame(deltaTime => {
-    thetaRef.current += omega * deltaTime
+    thetaRef.current += omegaRef.current * deltaTime
 
-    if (context) {
-      context.clearRect(0, 0, size, size)
+    if (ctx) {
+      ctx.clearRect(0, 0, canvasSize, canvasSize)
 
-      context.fillStyle = 'currentColor'
-      context.textAlign = 'center'
-      context.textBaseline = 'middle'
-      context.font = `${Math.round(size * 0.05)}px Inter`
+      ctx.fillStyle = 'currentColor'
+      ctx.textAlign = 'center'
+      ctx.textBaseline = 'middle'
+      ctx.font = `${Math.round(canvasSize * 0.05)}px Inter`
 
-      for (const { item, position } of points) {
+      for (const { item, position } of pointsRef.current) {
         const rotated = rotateAroundUnitVector(position, axisRef.current, thetaRef.current)
         const projected = worldToCamera(rotated, projection)
 
-        context.save()
-        context.translate(projected.x, projected.y)
-        context.scale(projected.scale, projected.scale)
-        context.globalAlpha = (sphereRadius - rotated.z) / (2 * sphereRadius)
-        context.fillText(item, 0, 0)
-        context.restore()
+        ctx.save()
+        ctx.translate(projected.x, projected.y)
+        ctx.scale(projected.scale, projected.scale)
+        ctx.globalAlpha = (sphereRadius - rotated.z) / (2 * sphereRadius)
+        ctx.fillText(item, 0, 0)
+        ctx.restore()
       }
     }
   })
 
-  return <canvas ref={canvasRef} width={size} height={size} />
+  return (
+    <canvas
+      className="touch-none cursor-grab active:cursor-grabbing select-none"
+      ref={canvasRef}
+      width={canvasSize}
+      height={canvasSize}
+    />
+  )
 }
--- src/shared/fibonacciSphere.ts
+++ src/shared/fibonacciSphere.ts
@@ -1,17 +1,21 @@
-const { sin, cos, acos, PI } = Math
+const { sin, asin, cos, acos, PI } = Math
 
 const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2
 
-interface Pos3D {
+export interface Pos3D {
   x: number
   y: number
   z: number
 }
 
-interface PerspectiveOptions {
-  perspective: number
-  cx: number
-  cy: number
+interface Pos2D {
+  x: number
+  y: number
+}
+
+interface Projection {
+  cameraZ: number
+  canvasSize: number
 }
 
 export function rotateAroundUnitVector(pos: Pos3D, axis: Pos3D, angle: number) {
@@ -42,13 +46,77 @@ export const initialPositionsOf = <T>(items: readonly T[], radius: number = 1) =
     }
   })
 
-export function worldToCamera({ x, y, z }: Pos3D, { perspective, cx, cy }: PerspectiveOptions) {
-  const scale = perspective / (perspective + z)
+export function worldToCamera(
+  { x, y, z }: Pos3D,
+  { cameraZ, canvasSize }: Projection
+): Pos2D & { scale: number } {
+  const scale = cameraZ / (cameraZ + z)
   return {
     scale,
-    x: cx + x * scale,
-    y: cy + y * scale,
+    x: canvasSize / 2 + x * scale,
+    y: canvasSize / 2 + y * scale,
   }
+}
+
+function cameraToWorld(
+  { x, y }: Pos2D,
+  targetZ: number,
+  { cameraZ, canvasSize }: Projection
+): Pos3D {
+  const scale = cameraZ / (cameraZ + targetZ)
+  return {
+    x: (x - canvasSize / 2) / scale,
+    y: (y - canvasSize / 2) / scale,
+    z: targetZ,
+  }
+}
+
+const Vec = Object.freeze({
+  dot: (v: Pos3D, w: Pos3D): number => v.x * w.x + v.y * w.y + v.z * w.z,
+  cross: (v: Pos3D, w: Pos3D): Pos3D => ({
+    x: v.y * w.z - v.z * w.y,
+    y: v.z * w.x - v.x * w.z,
+    z: v.x * w.y - v.y * w.x,
+  }),
+  len: (v: Pos3D) => Math.sqrt(Vec.dot(v, v)),
+  scale: (v: Pos3D, n: number): Pos3D => ({ x: v.x * n, y: v.y * n, z: v.z * n }),
+  add: (v: Pos3D, w: Pos3D): Pos3D => ({ x: v.x + w.x, y: v.y + w.y, z: v.z + w.z }),
+  sub: (v: Pos3D, w: Pos3D): Pos3D => ({ x: v.x - w.x, y: v.y - w.y, z: v.z - w.z }),
+})
+
+export function pointerToSpherePoint(
+  pointerPos: Pos2D,
+  sphereRadius: number,
+  projection: Projection
+): Pos3D {
+  const start = cameraToWorld(pointerPos, projection.cameraZ, projection)
+  const end = cameraToWorld(pointerPos, projection.cameraZ + 1, projection)
+  const ray = Vec.sub(end, start)
+
+  const a = Vec.dot(ray, ray)
+  const c = Vec.dot(start, start) - sphereRadius * sphereRadius
+  const b = Vec.dot(end, end) - a - c - sphereRadius * sphereRadius
+
+  const deltaSqrt = Math.sqrt(b * b - 4 * a * c)
+  if (deltaSqrt >= 0) {
+    return Vec.add(start, Vec.scale(ray, (-b - deltaSqrt) / (2 * a)))
+  }
+
+  const point = Vec.add(start, Vec.scale(ray, -Vec.dot(start, ray) / a))
+  return Vec.scale(point, sphereRadius / Vec.len(point))
+}
+
+export function findRotation(from: Pos3D, to: Pos3D) {
+  const cross = Vec.cross(from, to)
+  const len = Vec.len(cross)
+
+  if (len < 0.0001) {
+    return { axis: { x: 1, y: 0, z: 0 }, theta: 0 }
+  }
+
+  const theta = asin(len / (Vec.len(from) * Vec.len(to)))
+  const axis = Vec.scale(cross, 1 / len)
+  return { axis, theta }
 }
 
 export const INITIAL_ROTATION_AXIS = { x: -Math.SQRT1_2, y: -Math.SQRT1_2, z: 0 }
