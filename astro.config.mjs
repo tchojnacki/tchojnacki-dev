bc1b71f160af00291a356ecf53d568c157c90664
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-09-30 19:00:20 +0200

    feat: integrate with Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
+// @ts-check
+
 import { defineConfig } from 'astro/config';
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: vercelServerless()
+});
