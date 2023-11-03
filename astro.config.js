ef414a77237a49e98e98fb67dbd188729dab6fc3
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-11-03 17:24:23 +0100

    feat: add a sitemap

--- astro.config.js
+++ astro.config.js
@@ -3,11 +3,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import robotsTxt from 'astro-robots-txt'
 import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tchojnacki.dev',
-  integrations: [tailwind(), react(), robotsTxt(), compress()],
+  integrations: [tailwind(), react(), sitemap(), robotsTxt(), compress()],
 })
