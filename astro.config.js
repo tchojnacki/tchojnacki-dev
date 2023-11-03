407136fa547c2a503b8859212547f5ac15c808a3
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-11-03 16:22:54 +0100

    feat: add missing pages, remove hybrid rendering

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import robotsTxt from 'astro-robots-txt'
@@ -10,7 +9,5 @@ import compress from 'astro-compress'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tchojnacki.dev',
-  output: 'hybrid',
-  adapter: vercel(),
   integrations: [tailwind(), react(), robotsTxt(), compress()],
 })
