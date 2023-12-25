7b2b48425c35df6f64b4587d39a26d82bea2fcc6
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-12-25 12:58:28 +0100

    build: exclude utils from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,9 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config'
 export default getViteConfig({
-  test: {},
+  test: {
+    coverage: {
+      include: ['src/logic'],
+    },
+  },
 })
