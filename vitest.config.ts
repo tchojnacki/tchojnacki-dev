15904a9847db258916f37144abe5b9e728e7dbab
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-01-23 13:01:42 +0100

    fix: temporarily ignore TS error

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 /// <reference types="vitest" />
+
 import { getViteConfig } from 'astro/config'
+
 export default getViteConfig({
+  // @ts-expect-error ts(2353) - TODO: remove after dependency update
   test: {
     coverage: {
       include: ['src/logic'],
