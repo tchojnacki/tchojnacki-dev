e55d2215f0d140faa0d0a61bb3d9945e812328db
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-01-23 13:21:53 +0100

    build: make breaking changes to client dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@
 import { getViteConfig } from 'astro/config'
 
 export default getViteConfig({
-  // @ts-expect-error ts(2353) - TODO: remove after dependency update
   test: {
     coverage: {
       include: ['src/logic'],
