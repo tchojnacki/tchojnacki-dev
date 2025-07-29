ca5fa80ef3b4648cb58f62e3ede7f57d0915a34c
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-07-29 23:02:30 +0200

    refactor: remove dependency on astro-font

--- astro.config.js
+++ astro.config.js
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import compress from 'astro-compress'
 import robotsTxt from 'astro-robots-txt'
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 import { remarkReadingTime } from './src/utils/blog/readingTime'
 
@@ -17,4 +17,15 @@ export default defineConfig({
   integrations: [react(), mdx(), sitemap(), robotsTxt(), compress({ HTML: false })],
   markdown: { remarkPlugins: [remarkReadingTime] },
   vite: { plugins: [tailwindcss()] },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+        weights: ['400 600'],
+        subsets: ['latin', 'latin-ext'],
+      },
+    ],
+  },
 })
