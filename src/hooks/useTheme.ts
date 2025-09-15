b1920b17f92f089f4b49339ee3adcdd2f16708e8
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-09-15 18:22:02 +0200

    refactor: simplify theme handling

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,34 +1,32 @@
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 function initialTheme() {
-  if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
-    const local = localStorage.getItem("theme")
-    if (local === "dark" || local === "light") {
-      return local
-    }
-  }
-  return "dark"
+  if (typeof localStorage === "undefined") return "dark"
+  const local = localStorage.getItem("theme")
+  if (local !== "dark" && local !== "light") return "dark"
+  return local
 }
 
 function adjustHtmlBackground(theme: string) {
   var scroll = document.body.scrollTop || document.documentElement.scrollTop
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight
 
-  if (scroll / height > 0.5) {
-    document.documentElement.style.setProperty(
-      "background-color",
-      theme === "dark" ? "#282566" : "#e0e7ff",
-    )
-  } else {
-    document.documentElement.style.setProperty(
-      "background-color",
-      theme === "dark" ? "#14122b" : "#f7f9ff",
-    )
-  }
+  const backgroundColor =
+    scroll / height > 0.5
+      ? theme === "dark"
+        ? "#282566"
+        : "#e0e7ff"
+      : theme === "dark"
+        ? "#14122b"
+        : "#f7f9ff"
+  const themeColor = theme === "dark" ? "#14122b" : "#f7f9ff"
+
+  document.documentElement.style.setProperty("background-color", backgroundColor)
+  document.querySelector("meta[name=theme-color]")?.setAttribute("content", themeColor)
 }
 
 export function useTheme() {
-  const [theme, setTheme] = useState(initialTheme())
+  const [theme, setTheme] = useState(initialTheme)
 
   useEffect(() => {
     if (theme === "dark") {
@@ -38,14 +36,18 @@ export function useTheme() {
     }
 
     localStorage.setItem("theme", theme)
-
     adjustHtmlBackground(theme)
-    const listener = () => adjustHtmlBackground(theme)
+
+    function listener() {
+      adjustHtmlBackground(theme)
+    }
     document.addEventListener("scroll", listener)
     return () => document.removeEventListener("scroll", listener)
   }, [theme])
 
-  const toggleTheme = useCallback(() => setTheme(prev => (prev === "dark" ? "light" : "dark")), [])
+  function toggleTheme() {
+    setTheme(prev => (prev === "dark" ? "light" : "dark"))
+  }
 
   return { theme, toggleTheme }
 }
