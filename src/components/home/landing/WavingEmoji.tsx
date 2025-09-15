ef0c4a7144cb3ed8bb227ccde2950d93fdab0fed
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-09-15 17:19:31 +0200

    refactor(home): make WavingEmoji more idiomatic

--- src/components/home/landing/WavingEmoji.tsx
+++ src/components/home/landing/WavingEmoji.tsx
@@ -1,18 +1,12 @@
 import clsx from "clsx"
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 
 import Emoji from "~/components/common/Emoji"
-import { useEventListener } from "~/hooks"
 
 const WAVING_DELAY = 1000
 
 export default function WavingEmoji() {
   const [isPlaying, setIsPlaying] = useState(false)
-  const startPlaying = useCallback(() => setIsPlaying(true), [])
-  const stopPlaying = useCallback(() => setIsPlaying(false), [])
-
-  const ref = useRef<HTMLButtonElement>(null)
-  useEventListener(ref, "animationend", stopPlaying, { passive: true })
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -24,8 +18,8 @@ export default function WavingEmoji() {
 
   return (
     <button
-      ref={ref}
-      onClick={startPlaying}
+      onClick={() => setIsPlaying(true)}
+      onAnimationEnd={() => setIsPlaying(false)}
       className={clsx(
         "inline-block origin-[75%_75%] cursor-pointer",
         isPlaying && "motion-safe:animate-emojiwave",
